dddcbed32149503627af2f2e4a62f038f0f8213b
sgorgun <sergei.gorgun@gmail.com>
2023-10-20 00:37:20 +0300

    Added Program.cs

--- interfacesLessonMyConsoleApp/Engine.cs
+++ interfacesLessonMyConsoleApp/Engine.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace interfacesLessonMyConsoleApp
+{
+}
--- interfacesLessonMyConsoleApp/Program.cs
+++ interfacesLessonMyConsoleApp/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace interfacesLessonMyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rocket rocket = new Rocket();
+            rocket.Header = new RocketHeader();
+
+            rocket.Engine = new HatersEngine();
+            Console.WriteLine("Heater pull:" + SpecialNasaMethod(rocket.Engine.Power, rocket.Weight));
+
+            Console.ReadKey();
+        }
+
+
+        public static int SpecialNasaMethod(int power, int mass)
+        {
+            return (mass / power * 10) + 82;
+        }
+    }
+}
