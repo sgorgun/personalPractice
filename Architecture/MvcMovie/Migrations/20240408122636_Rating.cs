e2d85ccd4d1f8cde018a305de2333997d3d144dd
sgorgun <sergei.gorgun@gmail.com>
2024-04-08 15:40:06 +0300

    The most significant changes involve the addition of a Rating property to the Movie model and the corresponding updates to the MoviesController, database, and views. A new ViewModel, MovieGenreViewModel, has been created to support filtering movies by genre and search string. The project's dependencies have also been updated with the addition of eslint.
    1. The Movie model in [Movie.cs] has been updated to include a Rating property. Data annotations have also been added to the Title, Genre, and Price properties for validation purposes.
    
    2. The MoviesController in [MoviesController.cs] has been updated. The Index method now includes parameters for movieGenre and searchString, allowing for movie filtering. The Create method has been updated to include the Rating property of the Movie model.
    
    3. A database migration has been performed, adding a Rating column to the Movie table. This is reflected in the files [20240408122636_Rating.Designer.cs, 20240408122636_Rating.cs].
    
    4. The context model snapshot in [MvcMovieContextModelSnapshot.cs] has been updated to reflect the changes in the Movie model, including the addition of the Rating property.
    
    5. A new ViewModel, MovieGenreViewModel, has been defined in [MovieGenreViewModel.cs]. This ViewModel includes a list of Movies, a SelectList of Genres, and properties for MovieGenre and SearchString.
    
    6. The SeedData class in [SeedData.cs] has been updated to include a Rating for each seeded Movie.
    
    7. The solution file [MvcMovie.sln] has been updated, possibly due to changes in the project structure or Visual Studio version.
    
    8. The Readme file [Readme.md] has been updated with a link to the ASP.NET MVC tutorial.
    
    9. The view files [Create.cshtml, Delete.cshtml, Details.cshtml, Edit.cshtml] have been updated to include a form field for the Rating property of the Movie model.
    
    10. The Index view file [Index.cshtml] has been updated to use the MovieGenreViewModel. It now includes a form for filtering movies by genre and search string, and it displays the Rating for each movie in the table.
    
    11. The `package.json` file has been updated with a new development dependency, `eslint` version `^9.0.0`. No changes have been made to the existing dependencies.

---
+++ Architecture/MvcMovie/Migrations/20240408122636_Rating.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class Rating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Movie",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Movie",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
---
+++ Architecture/MvcMovie/Readme.md
@@ -0,0 +1,6 @@
+https://dotnet.microsoft.com/en-us/apps/aspnet/mvc
+
+ASP.NET MVC Pattern
+A design pattern for achieving a clean separation of concerns
+https://learn.microsoft.com/aspnet/core/tutorials/first-mvc-app/?WT.mc_id=dotnet-35129-website
+
